7b8a8c7c46d952a7737096d375653d9bfccf47b9
VisualStudy <sparksoldier@outlook.com>
2026-04-09 21:46:30 +0900

    first commit

--- pyOOP/pyOOP.py
+++ pyOOP/pyOOP.py
@@ -1,3 +1,12 @@
-﻿class FirstClass:
-    def __init__(self, name, age, grade):
-       
+﻿class FirstClass: # 클래스 작성
+    def __init__(self, name, age, grade): # 초기화 메서드. 사실상 생성자 취급
+       self.name = name
+       self.age = age
+       self.grade = grade
+
+p1 = FirstClass("david", 40, 4.5) # 개체 생성
+
+# 출력
+print(p1.name)
+print(p1.age)
+print(p1.grade)
