7b34dc8221d6f56d606b8a8a45453313e35b27bd
VisualStudy <sparksoldier@outlook.com>
2026-04-17 23:49:24 +0900

    tkinter

--- pyOOP/tk_gui.py
+++ pyOOP/tk_gui.py
@@ -1,8 +1,25 @@
 ﻿from tkinter import *
 
+def printing():
+    print("버튼이 눌렸다!")
+
+def funny():
+    a = entry.get()
+    print(a)
+
 root = Tk()
 root.geometry("300x200")
 
+label = Label(root, text="이것은 라벨이다")
+label.pack()
 
+button = Button(root, text="출력", command=printing)
+button.pack()
+
+entry = Entry(root)
+entry.pack()
+
+but = Button(root, text="엔트리출력", command=funny)
+but.pack()
 
 root.mainloop()
