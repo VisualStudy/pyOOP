357daeb489f6e7103bb7a7f22f866f9c28bd3af6
VisualStudy <sparksoldier@outlook.com>
2026-04-18 15:43:35 +0900

    class

---
+++ pyOOP/tk_class.py
@@ -0,0 +1,12 @@
+﻿class World:
+    def __init__(self, people, country):
+        self.popular = people
+        self.nation = country
+
+    def announce(self):
+        print(f"우리 세계의 인구 수는 {self.popular}명입니다.")
+        print(f"우리 나라의 이름은 {self.nation}입니다.")
+
+earth = World(10000000, "보헤미아")
+
+earth.announce()
