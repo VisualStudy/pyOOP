158d1950da235063e043f93477a1c23d84ac596d
VisualStudy <sparksoldier@outlook.com>
2026-04-20 00:03:46 +0900

    private

---
+++ pyOOP/private_class.py
@@ -0,0 +1,20 @@
+﻿class Secret:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def name_get(self):
+        return self.__name
+
+    def name_set(self, name):
+        self.__name = name
+
+    def age_get(self):
+        return self.__age
+
+    def age_set(self, age):
+        self.__age = age
+
+a1 = Secret("byum", 21)
+
+print(a1.age_get())
