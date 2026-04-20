af8a621843c425c83f464267ddf1b2f1eba1eb00
VisualStudy <sparksoldier@outlook.com>
2026-04-20 13:40:26 +0900

    class

---
+++ pyOOP/class_magic.py
@@ -0,0 +1,16 @@
+﻿class Plant:
+    def __init__(self, name = "식물이", height = 0):
+        self.name = name
+        self.height = height
+
+    def __str__(self):
+        return "나는 식물!"
+
+    def growth(self, rain):
+        if rain == True:
+            self.height += 1
+            print(f"키가 자라서 {self.height}가 되었다!")
+
+p1 = Plant("다육이", 10)
+
+print(p1.name)
