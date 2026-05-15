3434ad85fde49e8651390660472c54e256a2da3e
VisualStudy <sparksoldier@outlook.com>
2026-05-15 22:07:55 +0900

    The Factorial of n with recursive function

---
+++ pyOOP/recursive_factorial.py
@@ -0,0 +1,8 @@
+def facto(num):
+    if num == 0 or num == 1:
+        return 1
+    return num * facto(num - 1)
+
+n = int(input("Enter a number: "))
+
+print(f"The factorial of {n} is: {facto(n)}")
