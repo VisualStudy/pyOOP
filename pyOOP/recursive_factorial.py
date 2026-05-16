e79e00f5589222910d730a0a1f21c4af706710d4
VisualStudy <sparksoldier@outlook.com>
2026-05-16 22:14:58 +0900

    end=

--- pyOOP/recursive_factorial.py
+++ pyOOP/recursive_factorial.py
@@ -1,6 +1,8 @@
 def facto(num):
     if num == 0 or num == 1:
+        print("1")
         return 1
+    print(f"{num}", end=" * ")
     return num * facto(num - 1)
 
 n = int(input("Enter a number: "))
