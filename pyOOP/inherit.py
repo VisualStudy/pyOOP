2126bd0bfef25cba01ba3a0d7544da5265bdd65b
VisualStudy <sparksoldier@outlook.com>
2026-06-03 23:25:37 +0900

    inherit.py, main_fibo.py 추가 및 시작 파일 변경
    inherit.py와 main_fibo.py 파일을 프로젝트에 추가하고, 시작 파일을 recursive_factorial.py에서 inherit.py로 변경하였습니다. Animal과 Dog 클래스를 활용한 상속 예제와 fibo 모듈을 이용한 피보나치 수열 출력 예제가 포함되어 있습니다.

---
+++ pyOOP/inherit.py
@@ -0,0 +1,14 @@
+﻿class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print("동물이 소리를 냅니다.")
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name}가 멍멍 짓습니다.")
+
+dog = Dog("밀크")
+
+dog.speak()
