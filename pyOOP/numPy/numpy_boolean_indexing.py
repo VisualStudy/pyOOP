d069933ec58c227a85e0e400ed86867dc1b71b17
VisualStudy <sparksoldier@outlook.com>
2026-06-08 00:00:20 +0900

    numpy 불리언 인덱싱 예제 파일 추가 및 프로젝트 설정
    pyOOP.pyproj에 numpy_boolean_indexing.py를 컴파일 대상으로 추가하고, 시작 파일을 해당 파일로 변경했습니다. numpy 배열에서 불리언 인덱싱을 활용하는 예제 코드를 포함한 numpy_boolean_indexing.py 파일을 새로 생성했습니다.

---
+++ pyOOP/numPy/numpy_boolean_indexing.py
@@ -0,0 +1,10 @@
+﻿import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print(arr > 30)
+# 결과: [False False False  True  True]
+
+# 다시 배열에 넣어 값 뽑기
+print(arr[arr > 30])
+# 이때 파이썬의 and, or 사용 불가: & 등 사용
