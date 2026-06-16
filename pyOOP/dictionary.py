b5ed5306e0b4012d9ebd5ff92eb0f4f491e94a1d
VisualStudy <sparksoldier@outlook.com>
2026-06-16 21:10:57 +0900

    dictionary.py에 리스트/배열 예제 추가 및 시작 파일 변경
    dictionary.py에 리스트 연산 및 numpy 배열 예제 코드를 추가하였고, pyOOP.pyproj의 시작 파일을 main.py에서 dictionary.py로 변경하였습니다.

--- pyOOP/dictionary.py
+++ pyOOP/dictionary.py
@@ -2,3 +2,10 @@
 
 MyList = [10, "A", 30, 'B' ,50]
 MyArray = np.array([10, 20, 30, 40, 50])
+
+MyList1 = MyList * 2
+print(MyList1)
+
+# MyList2 = MyList + 1
+MyList2 = MyList + [1]
+print(MyList2)
