4b74cd4dfa6f075dc299fceedb32358b129d67da
VisualStudy <sparksoldier@outlook.com>
2026-06-20 18:25:56 +0900

    Vehicle 및 Truck 클래스 추가 및 프로젝트 파일 갱신
    pyOOP.pyproj에 vehicle_super_non_super.py를 추가하였고, 해당 파일에 Vehicle 클래스와 이를 상속하는 Truck 클래스를 구현했습니다. Vehicle은 기본 정보와 getter/setter를, Truck은 payload 속성과 관련 메서드를 포함합니다.

---
+++ pyOOP/inheritance/vehicle_super_non_super.py
@@ -0,0 +1,23 @@
+﻿class Vehicle:
+    def __init__(self, make, model, color, price):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.price = price
+
+    def setMake(self, make):
+        self.make = make
+
+    def getMake(self):
+        return self.make
+
+class Truck(Vehicle):
+    def __init__(self, make, model, color, price, payload):
+        super().__init(make, model, color, price)
+        self.payload = payload
+
+    def setPayload(self, payload):
+        self.payload = payload
+
+    def getPayload(self):
+        return self.payload
