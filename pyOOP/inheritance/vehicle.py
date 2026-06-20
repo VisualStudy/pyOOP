ebcb68fea252a23afbbea50e9dd3e22df638fa0b
VisualStudy <sparksoldier@outlook.com>
2026-06-20 17:48:03 +0900

    Vehicle 예제 코드 및 프로젝트 설정 추가
    pyOOP.pyproj의 시작 파일을 inheritance/vehicle_super.py로 변경하고, vehicle.py 및 vehicle_super.py 파일을 프로젝트에 추가했습니다. vehicle.py에는 상속과 메서드 오버라이딩 예제가, vehicle_super.py에는 super()를 활용한 생성자 호출 예제가 포함되어 있습니다.

---
+++ pyOOP/inheritance/vehicle.py
@@ -0,0 +1,14 @@
+﻿class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self):
+        print(f"{self.name} 이동!")
+
+class Car(Vehicle):
+    def honk(self):
+        print("빵빵!")
+
+car = Car("붕붕")
+car.move()
+car.honk()
---
+++ pyOOP/inheritance/vehicle_super.py
@@ -0,0 +1,13 @@
+﻿class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+class Car(Vehicle):
+    def __init__(self, name, fuel):
+        super().__init__(name)
+        self.fuel = fuel
+
+car = Car("자동차", "경유")
+
+print(car.name)
+print(car.fuel)
