76429f0ca2d1914b5a88fc1e8ab84c7e5eb0d687
VisualStudy <sparksoldier@outlook.com>
2026-06-20 17:57:27 +0900

    Vehicle 및 Car 생성자에 실행 메시지 출력 추가
    Vehicle 클래스와 Car 클래스의 생성자에 각각 생성자 실행을 알리는 print문을 추가하여 객체 생성 시 동작을 명확히 확인할 수 있도록 개선하였습니다.

--- pyOOP/inheritance/vehicle_super.py
+++ pyOOP/inheritance/vehicle_super.py
@@ -1,11 +1,13 @@
 ﻿class Vehicle:
     def __init__(self, name):
         self.name = name
+        print("Vehicle 생성자 실행")
 
 class Car(Vehicle):
     def __init__(self, name, fuel):
         super().__init__(name)
         self.fuel = fuel
+        print("Car 생성자 실행")
 
 car = Car("자동차", "경유")
 
